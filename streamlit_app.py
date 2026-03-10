769bea4e8c14f00db895dbf683493a3482fd07ba
Teresa Noble <teresa.noble10@gmail.com>
2026-03-10 03:51:18 +0000

    change link title

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,5 @@ import streamlit as st
 st.title("Quiz")
 
 st.markdown('<iframe src="https://claude.site/public/artifacts/b2598d60-6141-42dd-88a1-6bd2c861fc9c/embed" title="Claude Artifact" width="100%" height="600" frameborder="0" allow="clipboard-write" allowfullscreen></iframe>', unsafe_allow_html=True)
+
+st.link_button("Quick Code Quiz", "https://codepowerappsquiz.streamlit.app/")
